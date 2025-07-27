96b3beed2166a78f363dd1e36a0f25b13ceaecce
Alan Yeung <184780131+arlainn@users.noreply.github.com>
2025-07-27 20:24:36 +0800

    Create user options and basic file functionality

--- encrypted-notes.py
+++ encrypted-notes.py
@@ -3,10 +3,12 @@
 # Created by Alan Yeung
 # July 21, 2025
 
+import os
+
 # Use a flag to keep main loop running until user decides to quit
 keep_running = True
 
-while (keep_running):
+while keep_running:
     print('Welcome to Encrypted Notes. What would you like to do?\n'
         '\t1. Create a new note\n'
         '\t2. Open an existing note\n'
@@ -16,3 +18,30 @@ while (keep_running):
     print('Select an option: ', end='')
 
     user_option = input()
+
+    match user_option:
+        case '1':
+            print('Creating a new note.')
+
+            print('Enter a filename: ', end='')
+            filename = input()
+
+            print('Type your message here: ', end='')
+            message = input()
+
+            with open(filename + '.txt', 'w') as file:
+                file.write(message)
+        case '2':
+            print('Enter the name of the file to open: ', end='')
+
+            with open(input() + '.txt', 'r') as file:
+                print(file.read())
+        case '3':
+            print('Enter the name of the file to delete: ', end='')
+
+            os.remove(input() + '.txt')
+        case '4':
+            print('Exiting program.')
+            keep_running = False
+        case _:
+            print('Please select a valid option.')
